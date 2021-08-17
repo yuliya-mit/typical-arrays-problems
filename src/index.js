a7e80e5d242b1ed2e6374e71a2906f1c5ef32dbe
yuliya-mit <yulia.mititel@yandex.ru>
2021-08-17 17:40:33 +0300

    decision
    init: start task
    feat: add decision

--- src/index.js
+++ src/index.js
@@ -1,12 +1,48 @@
 
 exports.min = function min (array) {
-  return 0;
+  let arr=[];
+  for (item of arguments) {
+
+    if (typeof item !=='number' ) {
+     return 0 
+    }
+    else 
+    {
+      arr.push(item)}
+  }
+    arr.sort( (a, b) => a-b );
+    return arr[0];
+  
 }
 
 exports.max = function max (array) {
-  return 0;
+  let arr=[];
+  for (item of arguments) {
+
+    if (typeof item !=='number' ) {
+     return 0 
+    }
+    else 
+    {
+      arr.push(item)}
+  }
+    arr.sort( (a, b) => a-b );
+    return arr[arr.length-1];
+  
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let arr=[];
+  for (item of arguments) {
+
+    if (typeof item !=='number' ) {
+     return 0 
+    } else 
+    {
+      arr.push(item)}
+   
+  }
+    let sum=arr.reduce((sum, current) => sum + current, 0);
+    console.log(sum);
+    return sum/(arr.length-1);
 }
